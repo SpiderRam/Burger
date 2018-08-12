6f2fa7d249387e800304a3e3aa7db3bf7a48dfac
Carmen Owen <doctorsout@yahoo.com>
2018-08-11 21:39:57 -0400

    not even close

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ var PORT = process.env.PORT || 8090;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + '/public'));
+
 var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
